bf0a9f01ee2c8f98770a606651874dc85414c2c9
Brent Zukowski <59942057+bzukowski883@users.noreply.github.com>
2025-10-11 13:49:49 -0400

    Functional Event Creater
    Created a JS function to dynamically create a JSON entry for each individual day of a schedule to be uploaded.

--- Communal-Calendar/main.js
+++ Communal-Calendar/main.js
@@ -1,27 +1,46 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const fs = require('fs')
 
-const createWindow = () => {
+const createWindow = () => { //creates the actual electron window
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences:{
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   })
-
-  win.removeMenu()
-  win.loadFile('index.html')
+  win.webContents.openDevTools()    //opens inspect element if uncommented
+  win.removeMenu()    //removes windows hotbar for application
+  win.loadFile('index.html');    //loads html
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();   //opens the window we previously created
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();   
   })
 })
 
-ipcMain.on('form-submission', function(event, EventName){
-  console.log("this is the event's name ->", EventName)
+//recieves data from create event button
+ipcMain.handle('create-event', async (event, eventName, startDate, startTime, endTime, endDate, privacy, repeat, friendsString, separationStr) => {
+  friends = friendsString.split(separationStr)    //splits string into array because API can't handle arrays
+  friends.pop();    //pops the last element because it is a null entry (split)
+  eventObj = {eventName: eventName,startDate: startDate,startTime: startTime, endTime: endTime, endDate: endDate, privacy: privacy, repeat: repeat, friends: friends};    //creates an object to be JSONed
+  JSONEvent = JSON.stringify(eventObj);    //converts object to JSON
+  const filePath = "./test/output.txt";    //picks file for the JSON to be uploaded in
+  fs.appendFile(filePath, JSONEvent, 'utf8', (err) => {    //appends JSON to the specified file
+    if (err) {
+        // Handle errors
+        console.error('Error writing to file:', err.message);
+        return;
+    }
+    console.log(`File has been written successfully to ${filePath}`);
+});
 })
 
+//closes app on mac when the app is exited out of
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 })
--- Communal-Calendar/render.js
+++ Communal-Calendar/render.js
@@ -1,7 +1,35 @@
-const ipcRenderer = require('electron').ipcRenderer;
+const { ipcRenderer } = require('electron');
 
-function sendForm(event) {
-    event.preventDefault() // stop the form from submitting
-    let EventName = document.getElementById("Event").value;
-    ipcRenderer.send('form-submission', EventName)
+function formSubmit(event) {
+    event.preventDefault();    //prevents default behaviour of submit button
+    let friendsString = "";
+
+    //the string used in our split command so we can pass our accounts through the API
+    let separationStr = "@*#&^$@*#&($@#*(^%$*(@#&@#*%$*&@#)*^%&@*#$(@*#%^#@%*&@^!#&*^@#(&^$%*@$^*@%$*&^@#"    
+    let eventName = document.getElementById("Event").value;
+    let startDate = document.getElementById("Start-Date").value;
+    let startTime = document.getElementById("Start-Time").value;
+    let endTime = document.getElementById("End-Time").value;
+    let endDate = document.getElementById("End-Date").value;
+    let privacy = document.querySelector("input[name=Privacy]:checked").id;
+    let repeat = document.getElementById("Repeat").value;
+    let friends = Array.from(document.querySelectorAll('input[name=Friend]:checked'))
+                   .map(friend => friend.id);    //maps our selected friends IDs to an array to be stored in js
+    if (friends == null){
+        friends = false;    //if no friends are selected return false
+    }else{
+        for(const friend in friends){//convert friend array into string w/ separator 
+            friendsString += friends[friend] + separationStr;
+        }
+    }
+    if (endDate == ""){
+        endDate = startDate;    //if no endDate is selected return startDate
+    }
+
+    //send all this data to main with the code of 'create-event'
+    ipcRenderer.invoke('create-event', eventName, startDate, startTime, endTime, endDate, privacy, repeat, friendsString, separationStr).then((result)=>{
+    });
 }
+
+//add an event listener so we can throw the above js when the submit button is clicked
+document.getElementById('get-calendar-data').addEventListener("submit",formSubmit);
