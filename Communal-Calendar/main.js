9e39c32de32c13288ce84ecb896cc6d5a25929a6
Brent Zukowski <59942057+bzukowski883@users.noreply.github.com>
2025-10-27 14:08:04 -0400

    Added Cat Functionality and Fixed HTML
    Added functionality for categories, and fixed html nesting which was disturbing functionality, then changed css to adjust for those fixes

--- Communal-Calendar/main.js
+++ Communal-Calendar/main.js
@@ -25,11 +25,11 @@ app.whenReady().then(() => {
 
 //recieves data from create event button
 ipcMain.handle('create-event', async (event, eventName, startDate, startTime, endTime, endDate, privacy, repeat, friendsString, separationStr) => {
+  const filePath = "./test/output.txt";    //picks file for the JSON to be uploaded in
   friends = friendsString.split(separationStr)    //splits string into array because API can't handle arrays
   friends.pop();    //pops the last element because it is a null entry (split)
   eventObj = {eventName: eventName,startDate: startDate,startTime: startTime, endTime: endTime, endDate: endDate, privacy: privacy, repeat: repeat, friends: friends};    //creates an object to be JSONed
   JSONEvent = JSON.stringify(eventObj);    //converts object to JSON
-  const filePath = "./test/output.txt";    //picks file for the JSON to be uploaded in
   fs.appendFile(filePath, JSONEvent, 'utf8', (err) => {    //appends JSON to the specified file
     if (err) {
         // Handle errors
@@ -40,6 +40,18 @@ ipcMain.handle('create-event', async (event, eventName, startDate, startTime, en
 });
 })
 
+ipcMain.handle('retrive-catagories', async => {
+  const filePath = "./test/catagories.json" //picks file for json to be pulled from
+  const catagoriesJSON = fs.readFileSync(filePath, 'utf8', err => {
+    if (err) {
+        // Handle errors
+        console.error('Error reading file:', err.message);
+        return;
+    }
+    console.log(`File ${filePath} read from successfuly`);
+  })
+  return catagoriesJSON;
+})
 //closes app on mac when the app is exited out of
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
