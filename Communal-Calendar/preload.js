fa303023123dc23673cf0656e5fda4f35bfd191e
Brent Zukowski <59942057+bzukowski883@users.noreply.github.com>
2025-11-09 19:46:05 -0500

    Deleting Conflicting Files
    These files are conflicting so heavily they are un-useable

--- Communal-Calendar/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a secure API to the renderer
-contextBridge.exposeInMainWorld('electronAPI', {
-  sendForm: (firstname) => ipcRenderer.send('form-submission', firstname)
-});
