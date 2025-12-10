c8479631c859a6fdc225fa2379c74fe589c8fdf8
Elba <elbaalvarado26@gmail.com>
2025-12-09 21:38:26 -0500

    user info+picture displays, user can change pic and log out, login stays after app restart

--- Communal-Calendar/Communal-Calendars-App/preload.js
+++ Communal-Calendar/Communal-Calendars-App/preload.js
@@ -5,14 +5,21 @@
 */
 const { contextBridge, ipcRenderer } = require("electron"); //imports context bridge via electron
 contextBridge.exposeInMainWorld("electronAPI", {    //exposes certain api features to renderer
+  
   getCategories: (username, accessToken) => ipcRenderer.invoke("retrieve-categories", { username, accessToken }),
   getEvents: (username, accessToken) => ipcRenderer.invoke("get-events", { username, accessToken }),
   createEvent: (username, eventData, accessToken) => ipcRenderer.invoke("create-event", { username, eventData, accessToken }),
   loginUser: (username, password) => ipcRenderer.invoke("login-user", { username, password }),
   createCategory: (username, category, accessToken) => ipcRenderer.invoke("create-category", { username, category, accessToken }),
-  loginUser: (username, password) => ipcRenderer.invoke("login-user", { username, password}),
   registerUser: (email, username, password) => ipcRenderer.invoke("register-user", { email, username, password }),
   requestFriend: (requester, requestee, accessToken) => ipcRenderer.invoke("request-friend", { requester, requestee, accessToken}),
   acceptFriend: (requester, requestee, accessToken) => ipcRenderer.invoke("accept-friend", { requester, requestee, accessToken}),
-  denyFriend: (requester, requestee, accessToken) => ipcRenderer.invoke("deny-friend", { requester, requestee, accessToken})
-})
+  denyFriend: (requester, requestee, accessToken) => ipcRenderer.invoke("deny-friend", { requester, requestee, accessToken}),
+  
+  saveUserSession: (userData) => ipcRenderer.invoke("save-user-session", userData),
+  loadUserSession: () => ipcRenderer.invoke("load-user-session"),
+  clearUserSession: () => ipcRenderer.invoke("clear-user-session"),
+  selectProfilePicture: () => ipcRenderer.invoke("select-profile-picture"),
+  saveProfilePicture: (imageData) => ipcRenderer.invoke("save-profile-picture", imageData),
+  loadProfilePicture: () => ipcRenderer.invoke("load-profile-picture")
+});
